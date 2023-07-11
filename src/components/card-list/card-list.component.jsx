ec76930b48edd5c59d495fe1fb9582379213596f
James Abilong <james.abilong@awsys-i.com>
2023-07-11 19:46:44 +0800

    Changed from class to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,14 @@
+// import { Component } from "react";
+
 import "./card-list.styles.css";
 import Card from "../card/card.component";
 
-const CardList = (monsters) => {
+const CardList = ({ monsters }) => (
   <div className="card-list">
     {monsters.map((monster) => {
-      return <Card monsters={monster}></Card>;
+      return <Card monster={monster}></Card>;
     })}
-  </div>;
-};
+  </div>
+);
 
 export default CardList;
